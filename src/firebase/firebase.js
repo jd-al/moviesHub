9f398ee095913690c9e7d56a6d1f4e4abb311eb2
Jd <jd@al.dev>
2023-06-11 13:08:40 +0530

    firebase file updated

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -5,11 +5,11 @@ import { getFirestore, collection } from "firebase/firestore";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: "movieshub-fbee4.firebaseapp.com",
-  projectId: "movieshub-fbee4",
-  storageBucket: "movieshub-fbee4.appspot.com",
-  messagingSenderId: "624892072155",
-  appId: "1:624892072155:web:b2b8f90c3955398dc6bcfe",
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
